190a1505dc922e45b3426908b44a22add7797887
root <root@ubuntu.(none)>
2014-11-29 21:22:48 +0100

    updated Version from 0.0.1 to 0.0.2

--- ext_tables.php
+++ ext_tables.php
@@ -6,3 +6,9 @@ if (!defined('TYPO3_MODE')) {
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'xp CE FCE bootstrap');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:xpfcetmp/Configuration/TypoScript/page_ts.txt">');
+
+# translate 
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:cms/locallang_tca.xlf'][] = 'EXT:xpfcetmp/Resources/Private/Language/locallang_db.xlf';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['de']['EXT:cms/locallang_tca.xlf'][] = 'EXT:xpfcetmp/Resources/Private/Language/de.locallang_db.xlf';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['cn']['EXT:cms/locallang_tca.xlf'][] = 'EXT:xpfcetmp/Resources/Private/Language/cn.locallang_db.xlf';
+
